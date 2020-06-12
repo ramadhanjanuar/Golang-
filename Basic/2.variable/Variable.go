ec2c0733cdb94e7f1d892cd6a243a5b90026d86f
mochammadramadhanjanuar <mochammadramadhanjanuarh@gmail.com>
2020-06-12 21:38:13 +0700

    variable

---
+++ Basic/2.variable/Variable.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Ramadhan"
+	var secondName = "Januar"
+	var firstName, lastName string = "Mochammad", "Ramadhan"
+
+	fmt.Println(name, secondName, firstName, lastName)
+}
