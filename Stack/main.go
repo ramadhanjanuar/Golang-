6b2316310afd5774c56bd50c0ba62787d95ab825
mochammadramadhanjanuar <mochammadramadhanjanuarh@gmail.com>
2020-06-14 18:40:37 +0700

    add stack

--- Stack/main.go
+++ Stack/main.go
@@ -2,6 +2,49 @@ package main
 
 import "fmt"
 
+// Stack is stack
+type Stack []string
+
+// IsEmpty : check if stack is empty
+func (s *Stack) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+// Push a new value onto the stack
+func (s *Stack) Push(str string) {
+	*s = append(*s, str) // Simply append the new value to the end of the stack
+}
+
+// Pop Remove and return top element of stack. Return false if stack is empty.
+func (s *Stack) Pop() (string, bool) {
+	if s.IsEmpty() {
+		return "", false
+	} else {
+		index := len(*s) - 1   // Get the index of the top most element.
+		element := (*s)[index] // Index into the slice and obtain the element.
+		*s = (*s)[:index]      // Remove it from the stack by slicing it off.
+		return element, true
+	}
+}
+
+// Peek is Viewed the top most stact
+func (s *Stack) Peek() {
+	fmt.Println((*s)[0])
+}
+
 func main() {
-	fmt.Println("hello world")
+	var stack Stack // create a stack variable of type Stack
+
+	stack.Push("this")
+	stack.Push("is")
+	stack.Push("stack!!")
+	stack.Peek()
+
+	for len(stack) > 0 {
+		x, y := stack.Pop()
+		if y == true {
+			fmt.Println(x)
+		}
+	}
+
 }
